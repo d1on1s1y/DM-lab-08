15a9a8136569d569ba6fd39cd7a4b7f9d1788d4b
Денис Бурдин <dionisiy.god.of.wine@gmail.com>
2023-12-29 12:54:11 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,15 +3,15 @@ BNF = "<E>::=(<E>)|<E>+<E>|<E>*<E>|<V>|<C>"
 var1= "<V>::=x|y"
 var2= "<C>::=1|2"
 print(BNF + ", " + var1 + ", " + var2)
-def typeProd(STRarray):
+def typeOfProd(STRarray):
     res = []
     array = STRarray.replace("::=", "|").replace("<", "").replace(">", "").split("|")
     for el in array[1:len(array)]:
         res.append(array[0]+" -> "+el)
     print(", ".join(res))
-typeProd(BNF)
-typeProd(var1)
-typeProd(var2)
+typeOfProd(BNF)
+typeOfProd(var1)
+typeOfProd(var2)
 
 print(Fore.RED + "Ланцюжок: " + Fore.GREEN + "x+(x+1+2*x)")
 print(Fore.LIGHTGREEN_EX + "                   E             ")
@@ -31,7 +31,6 @@ print("                 V     C V     C    ")
 print("                 |     | |     |    ")
 print("                 x     1 x     2    ")
 print("                                     ")
-typeProd("x+x*(x+2)")
 
 
 
